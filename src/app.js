4b11dfde39d7d573778aa3b2cae6ecd6a911af03
Riza Fahmi <rizafahmi@gmail.com>
2019-02-14 17:26:35 +0700

    Introduce ESLint

--- src/app.js
+++ src/app.js
@@ -2,18 +2,26 @@ const {
   incrementQty,
   decrementQty,
   recalculateSubtotal
-} = require('./helpers.js');
+} = require("./helpers.js");
 
-const incrButton = document.querySelector('#incr');
-const decrButton = document.querySelector('#decr');
-const qtyInput = document.querySelector('#qty');
-const priceInput = document.querySelector('#price');
-const subtotalText = document.querySelector('#subtotal');
-const codeText = document.querySelector('#code');
-const confirmCodeButton = document.querySelector('#confirmCode');
+const incrButton = document.querySelector("#incr");
+const decrButton = 
+  document.querySelector("#decr");
+
+
+
+
+const qtyInput = document.querySelector("#qty");
+const priceInput = document.querySelector("#price");
+const subtotalText = document.querySelector("#subtotal");
+const codeText = document.querySelector("#code");
+const confirmCodeButton = document.querySelector("#confirmCode");
 let discount = 0;
 
-incrButton.addEventListener('click', () => {
+incrButton.addEventListener("click", () => {
+
+
+
   qtyInput.value = incrementQty(qtyInput.value);
   subtotalText.textContent = `Rp. ${recalculateSubtotal(
     priceInput.value,
@@ -22,7 +30,7 @@ incrButton.addEventListener('click', () => {
   )}`;
 });
 
-decrButton.addEventListener('click', () => {
+decrButton.addEventListener("click", () => {
   qtyInput.value = decrementQty(qtyInput.value);
   subtotalText.textContent = `Rp. ${recalculateSubtotal(
     priceInput.value,
@@ -31,8 +39,8 @@ decrButton.addEventListener('click', () => {
   )}`;
 });
 
-confirmCodeButton.addEventListener('click', () => {
-  if (codeText.value === 'KODE27') {
+confirmCodeButton.addEventListener("click", () => {
+  if (codeText.value === "KODE27") {
     discount = 27;
     subtotalText.textContent = `Rp. ${recalculateSubtotal(
       priceInput.value,
